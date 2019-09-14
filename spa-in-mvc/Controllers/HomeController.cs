737e90bc99ddd980a605bcbbbc8948deb6ab8beb
Felix Müller <muellefx@gmx.de>
2019-09-14 21:11:27 +0200

    Added view with bootstrapped angular sample app.

--- spa-in-mvc/Controllers/HomeController.cs
+++ spa-in-mvc/Controllers/HomeController.cs
@@ -25,5 +25,10 @@ namespace spa_in_mvc.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Angular()
+        {
+            return View();
+        }
     }
 }
