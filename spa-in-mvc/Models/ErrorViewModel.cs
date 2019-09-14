65cf7e91d2754497e926de3cac82de019ac09d1c
Felix Müller <muellefx@gmx.de>
2019-09-14 20:14:09 +0200

    Initial commit with ASP.NET Core 2.2 MVC project.

---
+++ spa-in-mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace spa_in_mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
